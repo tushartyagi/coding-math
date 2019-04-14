18c7b7c274061928537a97600f1ecf6eb500f2a1
Tushar Tyagi <tushar4r@gmail.com>
2019-04-14 13:37:01 +0530

    Keyboard bindings, Changing Acceleration & Flying ship

---
+++ src/acceleration.lisp
@@ -0,0 +1,80 @@
+(in-package :coding-math)
+
+(with-sketch accelerating-particle ((p (particle-create 10 height 10 (* -1 (/ PI 2))))
+                          (accel (make-vector 0.1 0.1)))
+  (flet ((update ()
+           (accelerate p accel)
+           (update p)))
+    (update)
+    (with-pen (make-pen :fill +red+ :stroke +black+)
+      (with-slots (position) p
+        (circle (x position) (y position) 3)))))
+
+(defsketch fireworks
+    ((width 1400) (height 1000)
+     (center-x (/ width 2)) (center-y (/ height 2))
+     (speed 12) (xs 0.01)
+     (particle-count 100)
+     (particles (fill-particles center-x center-y particle-count))
+     (gravity (make-vector 0 0.1)))
+  (background +white+)
+  (flet ((update-frame ()
+           (dotimes (i particle-count)
+             (accelerate (aref particles i) gravity)
+             (update (aref particles i)))))
+    (update-frame)
+    (with-pen (make-pen :fill +black+ :stroke +black+)
+      (iterate (for p in-vector particles)
+               (with-slots (position) p
+                 (circle (x position) (y position) 5))))))
+
+
+(with-sketch fireworks ((particle-count 100)
+                        (particles (fill-particles center-x center-y particle-count))
+                        (gravity (make-vector 0 0.1)))
+  (flet ((update-frame ()
+           (dotimes (i particle-count)
+             (accelerate (aref particles i) gravity)
+             (update (aref particles i)))))
+    (update-frame)
+    (with-pen (make-pen :fill +black+ :stroke +black+)
+      (iterate (for p in-vector particles)
+               (with-slots (position) p
+                 (circle (x position) (y position) 5))))))
+
+(with-sketch flying-ship ((ship (particle-create center-x center-y 0 0))
+                          (thrust (make-vector 0 0)))
+  (with-pen (make-pen :fill +black+ :stroke +black+)
+    (accelerate ship thrust)
+    (update ship)
+    (with-slots (position velocity) ship
+      (circle (x position) (y position) 5))))
+
+(defmethod kit.sdl2:keyboard-event ((window flying-ship) state timestamp repeat-p keysym)
+  (with-slots (thrust) window
+      (with-slots (x y) thrust
+        (when (eql state :keydown)
+          (cond 
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-up)
+             (setf y (- y 0.1)))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-down)
+             (setf y (+ y 0.1)))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-left)
+             (setf x (- x 0.1)))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-right)
+             (setf x (+ x 0.1)))))
+
+        (when (eql state :keyup)
+          (cond 
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-up)
+             (setf y 0))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-down)
+             (setf y 0))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-left)
+             (setf x 0))
+            ((sdl2:scancode= (sdl2:scancode-value keysym) :scancode-right)
+             (setf x 0))))
+        (print x))))
+
+
+
--- src/particle.lisp
+++ src/particle.lisp
@@ -4,13 +4,6 @@
   ((position :type vec :initarg :position)
    (velocity :type vec :initarg :velocity)))
 
-(defmethod print-object ((p particle) stream)
-  (print-unreadable-object (p stream :type t)
-    (with-slots (position velocity) p
-      (format stream "(~a, ~a)  (~a, ~a)"
-              (x position) (y position)
-              (len velocity) (angle velocity)))))
-
 (defun particle-create (x y speed direction)
   (let ((position (make-vector x y))
         (velocity (make-vector 0 0)))
@@ -18,6 +11,18 @@
     (setf (len velocity) speed)
     (make-instance 'particle :position position :velocity velocity)))
 
-(defmethod particle-update ((p particle))
+(defmethod print-object ((p particle) stream)
+  (print-unreadable-object (p stream :type t)
+    (with-slots (position velocity) p
+      (format stream "(~a, ~a)  (~a, ~a)"
+              (x position) (y position)
+              (len velocity) (angle velocity)))))
+
+;; TODO: Remove this particle word from the method names
+(defmethod update ((p particle))
   (with-slots (position velocity) p
     (setf position (v+ position velocity))))
+
+(defmethod accelerate ((p particle) (v vec))
+  (with-slots (velocity) p
+    (setf velocity (v+ v velocity))))
